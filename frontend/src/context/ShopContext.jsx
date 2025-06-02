ca8d042de58ba17b99473c66d11fb54c2f14e3a8
Taniya Sharma <taniya4115@gmail.com>
2025-06-02 23:43:41 +0530

    Initial commit

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+import { products } from "../assets/assets";
+import { useState } from "react";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = ( props ) => {
+  const currency = " ₹";
+  const dellivery_fee = 10;
+  const[search,setSearch]=useState('');
+  const[showSearch,setShowSearch]=useState(false);
+
+  const value = {
+    products,
+    currency,
+    dellivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch
+  };
+
+  return (
+    <ShopContext.Provider value={value}>
+      {props.children}
+      </ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
