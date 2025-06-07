3f6f33e4fbd3b8e1a870aadf52be048f45613643
Taniya Sharma <taniya4115@gmail.com>
2025-06-08 01:15:23 +0530

    Fix 'Proceed to Checkout' button placement

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -84,18 +84,20 @@ const Cart = () => {
                 onClick={() => updateQuantity(item._id, item.size, 0)}
                 className="w-4 mr-4 sm:w-5 cursor-pointer"
                 src={assets.bin_icon}
-                alt=""/>
+                alt=""
+              />
             </div>
           );
-        })
-        }
+        })}
       </div>
 
       <div className="flex justify-end my-20">
         <div className="w-full sm:w-[450px]">
-        <CartTotal />
+          <CartTotal />
+          <button className="bg-black text-white text-sm mt-5 px-8 py-3">
+            PROCEED TO CHECKOUT
+          </button>
         </div>
-
       </div>
     </div>
   );
