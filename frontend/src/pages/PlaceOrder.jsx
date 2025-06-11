d0ae89efa9818bc47b0f203a9238a17cbd405862
Taniya Sharma <taniya4115@gmail.com>
2025-06-11 23:43:13 +0530

    Fix: Payment method circle and layout

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -66,6 +66,7 @@ const PlaceOrder = () => {
           placeholder="Phone"
         />
       </div>
+
       {/* ----------------RIGHT SIDE---------------------- */}
       <div className="mt-8">
         <div className="mt-8 min-w-80">
@@ -74,54 +75,47 @@ const PlaceOrder = () => {
 
         <div className="mt-12">
           <Title text1={"PAYMENT"} text2={"METHOD"} />
-
           {/* --- Payment Method Selection --- */}
           <div className="flex gap-3 flex-col lg:flex-row">
             <div
               onClick={() => setMethod("stripe")}
               className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
             >
-              <p
-                className={`w-3.5 h-3.5 border rounded-full ${
+              <div
+                className={`w-5 h-5 border rounded-full ${
                   method === "stripe" ? "bg-green-400" : ""
                 }`}
-              ></p>
-              <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
+              ></div>
+              <img className="h-5" src={assets.stripe_logo} alt="" />
             </div>
             <div
               onClick={() => setMethod("razorpay")}
               className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
             >
-              <p
-                className={`w-3.5 h-3.5 border rounded-full ${
+              <div
+                className={`w-5 h-5 border rounded-full ${
                   method === "razorpay" ? "bg-green-400" : ""
                 }`}
-              ></p>
-              <img className="h-5 mx-4" src={assets.razorpay_logo} alt="" />
+              ></div>
+              <img className="h-5" src={assets.razorpay_logo} alt="" />
             </div>
             <div
               onClick={() => setMethod("cod")}
               className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
             >
-              <p
-                className={`w-3.5 h-3.5 border rounded-full ${
+              <div
+                className={`w-5 h-5 border rounded-full ${
                   method === "cod" ? "bg-green-400 " : ""
                 }`}
-              ></p>
-              <p className="text-gray-500 text-sm font-medium mx-4">
+              ></div>
+              <p className="text-gray-500 text-sm font-medium h-5 px-4">
                 CASH ON DELIVERY
               </p>
             </div>
-          </div>
-          <div className="w-full mt-8 flex justify-end">
-            <button className="bg-black text-white px-16 py-3 text-sm">
-              PLACE ORDER
-            </button>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-export default PlaceOrder
+export default PlaceOrder;
