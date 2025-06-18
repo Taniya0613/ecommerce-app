aa866b113824aaa5f4130286a07455119bc0a45f
Taniya Sharma <taniya4115@gmail.com>
2025-06-18 19:49:05 +0530

    Add navigation to orders page and import ShopContext in PlaceOrder

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/assets";
 import { useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
   const [method, setMethod] = useState("cod");
+
+  const { navigate } = useContext(ShopContext);
 
   return (
     <div className="mx-auto max-w-7xl px-5 flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t lg:gap-60">
@@ -114,10 +117,14 @@ const PlaceOrder = () => {
             </div>
           </div>
 
-           <div className='w-full text-end mt-8'>
-            <button className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
-           </div>
-
+          <div className="w-full flex justify-end mt-8">
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
+              PLACE ORDER
+            </button>
+          </div>
         </div>
       </div>
     </div>
